527b837666d7cdd66be37f87e88565b7ae3e028d
woodypark <np272@georgetown.edu>
2016-05-01 17:20:47 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inv <- NULL
+      set <- function(y) {
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function() x
+      set_inv <- function(i) inv <<- i
+      get_inv <- function() inv
+      list(set=set, get=get, set_inv=set_inv, get_inv=get_inv)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+         inv <- x$get_inv()
+      if(!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+      }
+      data <- x$get()
+      inv <- solve(data, ...)
+      x$set_inv(inv)
+      inv
 }
